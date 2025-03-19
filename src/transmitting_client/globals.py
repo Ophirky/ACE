60a125b701b7f45082f61516fe92c5e0abe285bd
Ophirky <ophirnevo3@gmail.com>
2025-03-19 23:25:07 +0200

    started transmitting client

---
+++ src/transmitting_client/globals.py
@@ -0,0 +1,18 @@
+"""
+Global variables
+"""
+from enum import Enum, auto
+
+class ErrorMessages(Enum):
+    OPEN_VIDEO_SOURCE = "Unable to open video source: %s"
+    VIDEO_CAPTURE_INIT = "Error initializing video capture for source {source}: {error}"
+
+    RETRIEVE_FRAME = "Failed to retrieve frame."
+    GET_FRAME_FROM_SOURCE = "Error retrieving frame from video source {source}: {error}"
+
+    RELEASE_SOURCE = "Error releasing video capture for source {source}: {error}"
+
+
+class SuccessMessages(Enum):
+    RETRIEVE_FRAME = "Frame retrieved successfully."
+    RELEASE_VIDEO_SOURCE = "Video source %s released successfully."
---
+++ src/transmitting_client/video_capture.py
@@ -0,0 +1,81 @@
+"""
+    This file holds the VideoCapture class
+"""
+
+# Imports
+import cv2
+import logging
+import numpy as np
+
+from globals import ErrorMessages, SuccessMessages
+
+class VideoCapture:
+    """
+    This class handles the video capture and handling.
+    """
+
+    def __init__(self, source=0) -> None:
+        """
+        Initializes the video capture instance.
+
+        :param source: (int or str): The video source, can be a camera index or a video file path.
+        :return: None
+        """
+        self.source = source
+
+        try:
+            self.capture = cv2.VideoCapture(self.source)
+            if not self.capture.isOpened():
+                logging.error(ErrorMessages.OPEN_VIDEO_SOURCE.value % self.source)
+                raise ValueError(ErrorMessages.OPEN_VIDEO_SOURCE.value % self.source)
+            
+            logging.info()
+
+        except Exception as e:
+            logging.exception(ErrorMessages.VIDEO_CAPTURE_INIT.value.format(source=self.source, error=e))
+            raise
+
+    def get_frame(self) -> tuple[bool, np.ndarray]:
+        """
+        Retrieves a single frame from the video source.
+
+        :return tuple: (success, frame), where `success` is a boolean indicating if the frame was read successfully, and `frame` is the captured frame.
+        """
+        res_tuple: tuple[bool, np.ndarray]
+        try:
+            success, frame = self.capture.read()
+            if success:
+                logging.debug(SuccessMessages.RETRIEVE_FRAME.value)
+            else:
+                logging.warning(ErrorMessages.RETRIEVE_FRAME.value)
+            return success, frame
+        
+        except Exception as e:
+            logging.exception(ErrorMessages.GET_FRAME_FROM_SOURCE.value.format(source=self.source, error=e))
+            res_tuple = (False, None)
+        
+        return res_tuple
+
+    def release(self) -> None:
+        """
+        Releases the video capture resource.
+
+        :return: None
+        """
+        try:
+            if self.capture:
+                self.capture.release()
+                logging.info(SuccessMessages.RELEASE_VIDEO_SOURCE.value % self.source)
+
+        except Exception as e:
+            logging.exception(ErrorMessages.RELEASE_SOURCE.value.format(source=self.source, error=e))
+
+    def __del__(self) -> None:
+        """
+        Ensures resources are released when the instance is deleted.
+
+        :return: None
+        """
+        self.release()
+        logging.info(SuccessMessages.RELEASE_VIDEO_SOURCE.value % self.source)
+
